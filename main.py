8c74746f40e52662301cac9f460dfddead2560be
[Abir Munna] <[mtmunna82@gmail.com]>
2023-06-18 00:27:08 +0600

    -updated cors policy

--- main.py
+++ main.py
@@ -14,9 +14,14 @@ IMAGEDIR = "images/"
 
 app = FastAPI()
 
+origins = [
+    "*"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=origins,
+    allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
 )
