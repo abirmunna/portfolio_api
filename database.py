2a41efa1026f273280ce34d8aa82245a8c97da47
abirmunna <mtmunna82@gmail.com>
2023-06-23 17:50:12 +0600

    fixed_databse issue.

--- database.py
+++ database.py
@@ -1,15 +1,16 @@
 from sqlalchemy import create_engine 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker 
+ 
+MYSQL_DATABASE_URL= "mysql+mysqlconnector://sql12628181:caL2G6bsgb@sql12.freemysqlhosting.net:3306/sql12628181"
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./tmp/sql_app.db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, 
-    connect_args={"check_same_thread" : False}
+    MYSQL_DATABASE_URL,
     )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
+mykey = "rrMby7BW_PRbjabFY9aUaFrahRoqVf5Kq3c6kCkpf"
--- models.py
+++ models.py
@@ -6,30 +6,30 @@ from database import Base
 class About(Base):
     __tablename__ = "about"
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True)
-    motto = Column(String, unique=True, index=True)
-    bio = Column(String, unique=True, index=True)
+    name = Column(String(255), unique=True, index=True)
+    motto = Column(String(255), unique=True, index=True)
+    bio = Column(String(255), unique=True, index=True)
 
 class Designation(Base):
     __tablename__ = "designation"
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True)
-    company = Column(String, unique=True, index=True)
-    location = Column(String, unique=True, index=True)
+    name = Column(String(255), unique=True, index=True)
+    company = Column(String(255), unique=True, index=True)
+    location = Column(String(255), unique=True, index=True)
 
 class Awards(Base):
     __tablename__ = "awards"
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, unique=True, index=True)
-    year = Column(String, unique=True, index=True)
+    title = Column(String(255), unique=True, index=True)
+    year = Column(String(255), unique=True, index=True)
 
 
 class Funding(Base):
     __tablename__ = "funding"
     id = Column(Integer, primary_key=True, index=True)
-    year = Column(String, unique=True, index=True)
-    title = Column(String, unique=True, index=True)
-    role = Column(String, unique=True, index=True)
-    awarded_amount = Column(String, unique=True, index=True)
-    time_period = Column(String, unique=True, index=True)
-    doner = Column(String, unique=True, index=True)
+    year = Column(String(255), unique=True, index=True)
+    title = Column(String(255), unique=True, index=True)
+    role = Column(String(255), unique=True, index=True)
+    awarded_amount = Column(String(255), unique=True, index=True)
+    time_period = Column(String(255), unique=True, index=True)
+    doner = Column(String(255), unique=True, index=True)
