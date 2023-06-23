f5d518dc41457e0b36aed98aeea81905b73f26b5
abirmunna <mtmunna82@gmail.com>
2023-06-23 18:11:21 +0600

    fixed cors policy

--- main.py
+++ main.py
@@ -21,9 +21,6 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*'],
 )
 
 def get_db():
