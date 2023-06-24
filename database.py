92c7ed9b0e74cb370e4a8df92947bef6fad920c0
MD ABIR HASAN MUNNA <mtmunna82@gmail.com>
2023-06-24 08:32:33 +0000

    used dotenv for better safety

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-POSTGRESQL_DATABASE_URL = "postgresql+psycopg2://abir:dCucnQWSLZ28WYQLH8t9csGGcQznXLLU@dpg-ciavd4mnqql51cdld0k0-a.singapore-postgres.render.com/portfolio_db_g9bu"
+load_dotenv()
+
+POSTGRESQL_DATABASE_URL = os.getenv("POSTGRESQL_DATABASE_URL")
 
 
 engine = create_engine(
@@ -13,4 +17,3 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-detaSpacekey = "rrMby7BW_PRbjabFY9aUaFrahRoqVf5Kq3c6kCkpf"
