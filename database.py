7cbc5b97a83e015807e4f838a3e9d01f76fbe657
MD ABIR HASAN MUNNA <mtmunna82@gmail.com>
2023-06-24 09:47:08 +0000

    fixed issue of .env not loading in hosted mode

--- database.py
+++ database.py
@@ -1,13 +1,13 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
+# from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv, find_dotenv
 import dotenv
 import os
 
-load_dotenv('./.env')
+load_dotenv('test.env')
 
-POSTGRESQL_DATABASE_URL = os.getenv("POSTGRESQL_DATABASE_URL")
+POSTGRESQL_DATABASE_URL = os.getenv("DB")
 
 print(POSTGRESQL_DATABASE_URL)
 engine = create_engine(
