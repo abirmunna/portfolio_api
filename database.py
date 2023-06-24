7e558cc04b11a3aa174ac55381bf2cda4898df13
MD ABIR HASAN MUNNA <mtmunna82@gmail.com>
2023-06-24 09:48:04 +0000

    formatted using black

--- database.py
+++ database.py
@@ -1,20 +1,18 @@
 from sqlalchemy import create_engine
+
 # from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv, find_dotenv
 import dotenv
 import os
 
-load_dotenv('test.env')
+load_dotenv("test.env")
 
 POSTGRESQL_DATABASE_URL = os.getenv("DB")
 
 print(POSTGRESQL_DATABASE_URL)
-engine = create_engine(
-    POSTGRESQL_DATABASE_URL
-)
+engine = create_engine(POSTGRESQL_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
