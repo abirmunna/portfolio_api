625052e8f6d56617840e9400fd092bb375d02a5d
MD ABIR HASAN MUNNA <mtmunna82@gmail.com>
2023-06-24 09:21:17 +0000

    trying to solve the dotenv issue in deta sapce

--- database.py
+++ database.py
@@ -2,15 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+import dotenv
 import os
 
-load_dotenv()
+load_dotenv('./.env')
 
 POSTGRESQL_DATABASE_URL = os.getenv("POSTGRESQL_DATABASE_URL")
 
-
+print(POSTGRESQL_DATABASE_URL)
 engine = create_engine(
-    POSTGRESQL_DATABASE_URL,
+    POSTGRESQL_DATABASE_URL
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
