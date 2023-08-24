b99eeaea707d4bbb7b1933066950d40638aa280c
[Abir Munna] <[mtmunna82@gmail.com]>
2023-08-24 12:50:00 +0600

    modified:   database.py

--- database.py
+++ database.py
@@ -17,15 +17,15 @@ engine = create_engine(POSTGRESQL_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-Session = SessionLocal()
-# # Add the new column to the table
-# Session.execute(text('ALTER TABLE research ADD COLUMN status VARCHAR(255)'))
-# Session.execute(text('ALTER TABLE publications ADD COLUMN publications_type VARCHAR(255)'))
-# alter the string size for About table motto column
+# Session = SessionLocal()
+# # # Add the new column to the table
+# # Session.execute(text('ALTER TABLE research ADD COLUMN status VARCHAR(255)'))
+# # Session.execute(text('ALTER TABLE publications ADD COLUMN publications_type VARCHAR(255)'))
+# # alter the string size for About table motto column
 # Session.execute(text('ALTER TABLE about ALTER COLUMN bio TYPE VARCHAR(25500)'))
-# Session.execute(text('ALTER TABLE research ALTER COLUMN description TYPE VARCHAR(25500)'))
-# Commit the changes
-Session.commit()
+# # Session.execute(text('ALTER TABLE research ALTER COLUMN description TYPE VARCHAR(25500)'))
+# # Commit the changes
+# Session.commit()
 
 
 
